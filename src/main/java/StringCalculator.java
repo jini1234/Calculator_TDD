604bc1abc101235da46333d88b66ec27a4782f59
jinisha <jinishat17@gmail.com>
2021-09-03 10:51:47 +0530

    Code after completion of requirement 4

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -3,12 +3,26 @@ public class StringCalculator {
         if (numbers.equals("")) {
             return 0;
         } else {
-            String[] numsString = numbers.split(",|\n");
+            String delimiter = ",";
+            if (numbers.startsWith("//")) {
+                delimiter = getDelimiter(numbers);
+                numbers = getNumbers(numbers);
+            }
+
+            String[] numsString = numbers.split(delimiter + "|\n");
             int sum = 0;
             for (String num : numsString) {
                 sum += Integer.parseInt(num);
             }
             return sum;
         }
+    }
+
+    private String getNumbers(String numbers) {
+        return numbers.substring(numbers.indexOf("\n") + 1);
+    }
+
+    private String getDelimiter(String numbers) {
+        return numbers.substring(2, numbers.indexOf("\n"));
     }
 }
