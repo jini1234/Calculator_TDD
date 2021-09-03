2b5e9cd41cc6b5643478762a0ce5a6026b04d15c
jinisha <jinishat17@gmail.com>
2021-09-03 10:25:19 +0530

    completion of first requirement

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -3,7 +3,16 @@ public class StringCalculator {
     public int Add(String numbers) {
         if (numbers.equals("")) {
             return 0;
+        } else {
+            String[] numsString = numbers.split(",");
+            int sum = 0;
+            if(numbers.length()==1){
+                sum=Integer.parseInt(numsString[0]);
+            }
+            else {
+                sum = Integer.parseInt(numsString[0]) + Integer.parseInt(numsString[1]);
+            }
+            return sum;
         }
-        return 0;
     }
 }
